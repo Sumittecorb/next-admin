144975a20eeeb0c3a76b0ec0eb6854ba432e6900
Gaurav Pandey <gaurav.2022@tecorb.co>
2023-07-21 15:43:46 +0530

    API- setup

---
+++ src/dbconfig/dbConfig.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+export async function connect() {
+    console.log(process.env.MONGO_URL,"URL");
+    
+    try {
+        mongoose.connect(process.env.MONGO_URL!)
+        const connection = mongoose.connection
+
+        connection.on('connected', () => {
+            console.log('mongodb connected successfully');
+        })
+        connection.on('error', (err) => {
+            console.log('mongoDB connection error. Please make sure MongoDB is running.' + err)
+            process.exit()
+        })
+    }
+    catch (error) {
+        console.log(error, "something gone wrong");
+
+    }
+}
