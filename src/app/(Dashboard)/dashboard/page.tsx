35f781741ee4cd43e666ca13585b4e77fcf88113
Gaurav Pandey <gaurav.2022@tecorb.co>
2023-07-24 19:26:54 +0530

    logout

--- src/app/(Dashboard)/dashboard/page.tsx
+++ src/app/(Dashboard)/dashboard/page.tsx
@@ -1,5 +1,24 @@
 "use client";
+
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
+
 const Home = () => {
+
+  const router = useRouter()
+
+  const handleLogout = async (e: any) => {
+    e.preventDefault()
+    try {
+      await axios.get("/api/user/logout")
+      router.push("/")
+    }
+    catch (error: any) {
+      console.log(error.message);
+    }
+  }
+
   return (
     <>
       <main className="relative h-full max-h-screen transition-all duration-200 ease-in-out xl:ml-68 rounded-xl">
@@ -29,7 +48,6 @@ const Home = () => {
                 Dashboard
               </h6>
             </nav>
-
             <div className="flex items-center mt-2 grow sm:mt-0 sm:mr-6 md:mr-0 lg:flex lg:basis-auto">
               <div className="flex items-center md:ml-auto md:pr-4">
                 <div className="relative flex flex-wrap items-stretch w-full transition-all rounded-lg ease">
@@ -59,11 +77,11 @@ const Home = () => {
                     href="./pages/sign-in.html"
                     className="block px-0 py-2 text-sm font-semibold text-white transition-all ease-nav-brand"
                   >
-                    <i className="fa fa-user sm:mr-1"></i>
-                    <span className="hidden sm:inline">Sign In</span>
+                    {/* <i className="fa fa-user sm:mr-1"></i> */}
+                    <span onClick={handleLogout} className="hidden sm:inline">Logout</span>
                   </a>
                 </li>
-                <li className="flex items-center pl-4 xl:hidden">
+                {/* <li className="flex items-center pl-4 xl:hidden">
                   <a
                     href="javascript:;"
                     className="block p-0 text-sm text-white transition-all ease-nav-brand"
@@ -75,8 +93,8 @@ const Home = () => {
                       <i className="ease relative block h-0.5 rounded-sm bg-white transition-all"></i>
                     </div>
                   </a>
-                </li>
-                <li className="flex items-center px-4">
+                </li> */}
+                {/* <li className="flex items-center px-4">
                   <a
                     href="javascript:;"
                     className="p-0 text-sm text-white transition-all ease-nav-brand"
@@ -85,13 +103,12 @@ const Home = () => {
                       fixed-plugin-button-nav
                       className="cursor-pointer fa fa-cog"
                     ></i>
-                    {/* <!-- fixed-plugin-button-nav  --> */}
                   </a>
-                </li>
+                </li> */}
 
                 {/* <!-- notifications --> */}
 
-                <li className="relative flex items-center pr-2">
+                {/* <li className="relative flex items-center pr-2">
                   <p className="hidden transform-dropdown-show"></p>
                   <a
                     href="javascript:;"
@@ -106,7 +123,6 @@ const Home = () => {
                     dropdown-menu
                     className="text-sm transform-dropdown before:font-awesome before:leading-default before:duration-350 before:ease lg:shadow-3xl duration-250 min-w-44 before:sm:right-8 before:text-5.5 pointer-events-none absolute right-0 top-0 z-50 origin-top list-none rounded-lg border-0 border-solid border-transparent dark:shadow-dark-xl dark:bg-slate-850 bg-white bg-clip-padding px-2 py-4 text-left text-slate-500 opacity-0 transition-all before:absolute before:right-2 before:left-auto before:top-0 before:z-50 before:inline-block before:font-normal before:text-white before:antialiased before:transition-all before:content-['\f0d8'] sm:-mr-6 lg:absolute lg:right-0 lg:left-auto lg:mt-2 lg:block lg:cursor-pointer"
                   >
-                    {/* <!-- add show class on dropdown open js --> */}
                     <li className="relative mb-2">
                       <a
                         className="dark:hover:bg-slate-900 ease py-1.2 clear-both block w-full whitespace-nowrap rounded-lg bg-transparent px-4 duration-300 hover:bg-gray-200 hover:text-slate-700 lg:transition-colors"
@@ -213,7 +229,7 @@ const Home = () => {
                       </a>
                     </li>
                   </ul>
-                </li>
+                </li> */}
               </ul>
             </div>
           </div>
