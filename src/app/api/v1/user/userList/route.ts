e8fd3e19e38fd0d8fd60db99c5ffb3691f03d49d
Sumit Saxena <sumit.2019@tecorb.co>
2023-07-31 18:07:54 +0530

    Make merge

--- src/app/api/v1/user/userList/route.ts
+++ src/app/api/v1/user/userList/route.ts
@@ -2,21 +2,17 @@ import { NextResponse, NextRequest } from "next/server";
 import User from "@/models/userModels";
 import { connect } from "@/dbconfig/dbConfig";
 
-connect()
+connect();
 
 export async function GET(request: NextRequest) {
-    try {
-        const userList = await User.find()
-        if (userList.length == 0) {
-            return NextResponse.json({ error: "No Data Found" }, { status: 404 })
-        }
-        else {
-            return NextResponse.json({ userList })
-        }
-
+  try {
+    const userList = await User.find();
+    if (userList.length == 0) {
+      return NextResponse.json({ error: "No Data Found" }, { status: 404 });
+    } else {
+      return NextResponse.json({ userList });
     }
-    catch (error: any) {
-        return NextResponse.json({ error: error.message }),
-            { status: 500 }
-    }
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }), { status: 500 };
+  }
 }
