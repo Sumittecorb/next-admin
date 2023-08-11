5bd5355e1ba8ac32420da6ce3ccba3c28b2c86d5
Gaurav Pandey <gaurav.2022@tecorb.co>
2023-08-11 16:06:55 +0530

    make it better

--- src/app/(frontend)/(Dashboard)/other/prorammingLanguage.tsx
+++ src/app/(frontend)/(Dashboard)/other/prorammingLanguage.tsx
@@ -8,30 +8,6 @@ function ProgrammingLanguage() {
         value: ""
     }])
 
-    const handleAdd = (e: any) => {
-        e.preventDefault()
-        const lengthValue = inputField.length
-        const newInputField = [...inputField, { id: lengthValue, value: "" }]
-        setInputField(newInputField)
-    }
-
-    // const handleAdd = (e: any) => {
-    //     e.preventDefault()
-    //     if (inputField[inputField.length - 1].value === "") {
-    //     }
-
-    //     const lengthValue = inputField.length
-    //     const newInputField = [...inputField, { id: lengthValue, value: "" }]
-    //     setInputField(newInputField)
-    // }
-
-    const handleInputChange = (index: number, event: any) => {
-        const newValue = event.target.value;
-        const newInputFields = [...inputField];
-        newInputFields[index].value = newValue;
-        setInputField(newInputFields);
-    }
-
     return (
         <div>
             <h3>Add Programming Lanaguage</h3>
@@ -45,7 +21,6 @@ function ProgrammingLanguage() {
                             <input
                                 type="text"
                                 value={field.value}
-                                onChange={() => { handleInputChange(index, event) }}
                                 className={`appearance-none w-full block bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white`}
                                 id={`language_${field.id}`}
                                 name={`language_${field.id}`}
@@ -65,7 +40,7 @@ function ProgrammingLanguage() {
                     </div>
                 ))}
                 <div className="text-center mt-5">
-                    <button onClick={handleAdd} type="button" className="bg-black mt-8 hover:bg-gray-600-700 text-white mb-5 content-center w-20 m-auto font-bold py-2 px-4 border border-white-700 rounded">+</button>
+                    <button type="button" className="bg-black mt-8 hover:bg-gray-600-700 text-white mb-5 content-center w-20 m-auto font-bold py-2 px-4 border border-white-700 rounded">+</button>
                 </div>
             </div>
             <div className="mt-10">
